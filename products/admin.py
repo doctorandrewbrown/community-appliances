255453469e2d84233449ad776db90d6b358d7eaa
andrew brown <dr.andrew.david.brown@gmail.com>
2024-02-20 12:24:31 +0000

    customises admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+# the two classes below extend the admin model to tell admin view what data to show
+
+class ProductAdmin(admin.ModelAdmin):
+    # tuple below
+    list_display = (
+        'name',
+        'category',
+        'description',
+        'price',
+        'image',
+    )
+# chose option to order admin view note tuple
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+# Register your models here. Note admin models defined above are registered 
+# alongside respective models
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
+
+    # corrects plural in admin view
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -15,13 +22,14 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-    # string representations of model 
+    # string representations of model
     def __str__(self):
         return self.name
