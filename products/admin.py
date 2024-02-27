bb7999aad3847731842ed753963c5958c6eda9d1
andrew brown <dr.andrew.david.brown@gmail.com>
2024-02-27 20:32:56 +0000

    add Certificate and Condition models

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Condition, Certificate
 
-# the two classes below extend the admin model to tell admin view what data to show
+# the classes below extend the admin model to tell admin view what data to show
 
-
+class CertificateAdmin(admin.ModelAdmin):
+    # tuple below note () brackets
+    list_display = (
+        'certificate',
+        'description',
+    )
 class ProductAdmin(admin.ModelAdmin):
-    # tuple below
+    # tuple below note () brackets
     list_display = (
         'name',
         'category',
@@ -23,9 +28,15 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
+
+class ConditionAdmin(admin.ModelAdmin):
+    list_display = ('grade',)
+
 # Register your models here. Note admin models defined above are registered 
 # alongside respective models
 
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Condition, ConditionAdmin)
+admin.site.register(Certificate, CertificateAdmin)
--- products/models.py
+++ products/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class Certificate(models.Model):
+    certificate = models.CharField(max_length=254)
+    description = models.TextField(null=True, blank=True)
+    
+    # string representation of model
+    def __str__(self):
+        return self.certificate
 
 
+class Condition(models.Model):
+    class Meta:
+        verbose_name_plural = "Condition"
+    grade = models.CharField(max_length=254)
+    description = models.TextField(null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    # string representations of model
+    def __str__(self):
+        return self.grade
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+  
 class Category(models.Model):
 
     # corrects plural in admin view
@@ -25,7 +48,11 @@ class Product(models.Model):
                                  on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    grade = models.ForeignKey('Condition', null=True, blank=True,
+                              on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    certificate = models.ForeignKey('Certificate', null=True, blank=True,
+                                    on_delete=models.SET_NULL)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
