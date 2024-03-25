aed888511f07c87527a816f83b0fabfb4a360012
andrew brown <dr.andrew.david.brown@gmail.com>
2024-03-25 17:45:58 +0000

    adds cart template and contexts

--- community_appliances/settings.py
+++ community_appliances/settings.py
@@ -85,6 +85,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',  # required by allauth
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
