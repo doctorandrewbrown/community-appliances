b48a495e3d515e9ccc9cba8d4d19142a98a85c57
andrew brown <dr.andrew.david.brown@gmail.com>
2024-04-24 10:49:02 +0000

    adds add to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -8,3 +8,17 @@ def view_cart(request):
     """ A view for users to see cart contents """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, product_id):
+    """ A view to add product to cart """
+    # redirect to current page ie product details
+    redirect_url = request.POST.get('redirect_url')
+
+    # look for cart in session if it already exists else create it
+    cart = request.session.get('cart', {})
+    request.session['cart'] = cart
+    cart[product_id] = 1
+    # print(request.session['cart'])
+    return redirect(redirect_url)
+    
