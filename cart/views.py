a38549bfb5343c787dca1daa47e965fd488307e6
andrew brown <dr.andrew.david.brown@gmail.com>
2024-05-01 11:59:10 +0000

    removes print statements

--- cart/views.py
+++ cart/views.py
@@ -13,7 +13,8 @@ def view_cart(request):
 def add_to_cart(request, product_id):
     """ A view to add product to cart """
 
-    # look for cart in session if it already exists else create empty session dict
+    # look for cart in session if it already
+    # exists else create empty session dict
     cart = request.session.get('cart', {})
     # update cart contents in session dict
     request.session['cart'] = cart
@@ -22,5 +23,18 @@ def add_to_cart(request, product_id):
     # existing id so same product can not be bought twice if attempted
     cart[product_id] = 1
     # redirect to current page (ie product details) when add to bag complete
+    redirect_url = request.POST.get('redirect_url')
+    return redirect(redirect_url)
+
+
+def remove_from_cart(request, product_id):
+    """ A view to remove product from cart """
+    # get cart fom session or create
+    cart = request.session.get('cart', {})
+    # pop product from cart
+    cart.pop(product_id)
+    # update session dict
+    request.session['cart'] = cart
+    # return user to updated cart
     redirect_url = request.POST.get('redirect_url')
     return redirect(redirect_url)
