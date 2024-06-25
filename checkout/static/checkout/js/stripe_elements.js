8781dbdd30e0bd10def85d5f054d30b36e7e4c26
andrew brown <dr.andrew.david.brown@gmail.com>
2024-06-25 12:17:53 +0000

    adds stripe  payment functionallity

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -8,6 +8,7 @@
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
+console.log(clientSecret)
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
--- checkout/views.py
+++ checkout/views.py
@@ -12,41 +12,45 @@ import stripe
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "Your cart is empty")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        return redirect(reverse('checkout_success'))
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "Your cart is empty")
+            return redirect(reverse('products'))
 
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
-    order_form = OrderForm()
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form, 
-        'stripe_public_key': 'pk_test_51PEsYP01b0caL7kaca7zrnbpbcgs0xhGoM1dF9VBWLr1CWtKCG0m5K2Mq9JXpkp6qh2akURWlEc01NAHKArotVeX00fQFLZ0v9',
-        'client_secret': 'test client secret',
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+        
+        order_form = OrderForm()
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form, 
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
 
-    }
-    #messages.success(request, 'Order successfully processed!')
-    return render(request, template, context)
+        }
+        
+        return render(request, template, context)
 
-def checkout_success(request, order_number):
-        """
-            Handle successful checkouts
-        """
+def checkout_success(request):
+    """
+        Handle successful checkouts
+    """
 
-        if 'cart' in request.session:
-            del request.session['cart']
+    if 'cart' in request.session:
+        del request.session['cart']
 
-            template = 'checkout/checkout_success.html'
+        template = 'checkout/checkout_success.html'
 
-            return render(request, template)
+    return render(request, template)
