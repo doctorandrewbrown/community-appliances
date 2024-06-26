ef252b66612d7df2fa5aa53f3d531f144f2fa4b5
andrew brown <dr.andrew.david.brown@gmail.com>
2024-06-26 10:13:23 +0000

    saves order form data to model

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,25 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     if request.method == 'POST':
+        bag = request.session.get('bag', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],}
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            #order = order_form.save(commit=False)
+            #pid = request.POST.get('client_secret').split('_secret')[0]
+            #order.stripe_pid = pid
+            #order.original_bag = json.dumps(bag)
+            order_form.save()
         return redirect(reverse('checkout_success'))
     else:
         cart = request.session.get('cart', {})
