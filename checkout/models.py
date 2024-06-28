ab6f792707596a082fead12c8cae5d369d142f80
andrew brown <dr.andrew.david.brown@gmail.com>
2024-06-28 10:38:58 +0000

    calculates order discount

--- checkout/models.py
+++ checkout/models.py
@@ -38,11 +38,11 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        if self.order_total > settings.DISCOUNT_THRESHOLD:
+            self.discount = self.order_total * settings.PERCENTAGE_DISCOUNT/ 100
         else:
-            self.delivery_cost = 0
-        self.grand_total = self.order_total + self.delivery_cost
+            self.discount = 0
+        self.grand_total = self.order_total - self.discount
         self.save()
 
     def save(self, *args, **kwargs):
