62e86ea3d5c22fc453fd93588320aa22ed8ce26c
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-07-16 15:42:39 +0000

    saves and prefills checkout form info

--- checkout/views.py
+++ checkout/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpR
 from django.contrib import messages
 from django.conf import settings
 from .forms import OrderForm
+from profiles.forms import UserProfileForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 from cart.contexts import cart_contents
 
 import stripe
@@ -40,7 +42,10 @@ def checkout(request):
                         )
                     # save to order_line_item model
                     order_line_item.save()
-            return redirect(reverse('checkout_success'))
+            
+            # Save the info to the user's profile if all is well
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
@@ -59,7 +64,25 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
         
-        order_form = OrderForm()
+        # Attempt to prefill the form with any info the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
         if not stripe_public_key:
             messages.warning(request, 'Stripe public key is missing. \
                 Did you forget to set it in your environment?')
@@ -73,10 +96,33 @@ def checkout(request):
         
         return render(request, template, context)
 
-def checkout_success(request):
+def checkout_success(request, order_number):
     """
         Handle successful checkouts
     """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
     if 'cart' in request.session:
         del request.session['cart']
