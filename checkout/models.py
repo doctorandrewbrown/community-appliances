a35d4b5227b5601d7453faedca7efb2ba15e8035
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-07-23 20:15:55 +0000

    alter required form fields and implement order delete

--- checkout/models.py
+++ checkout/models.py
@@ -14,13 +14,13 @@ class Order(models.Model):
                                      null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=True)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=False, blank=False, default='CF')
+    country = models.CharField(max_length=40, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, 
                                       decimal_places=2, null=False, default=0)
@@ -38,15 +38,19 @@ class Order(models.Model):
     def update_total(self):
         """
         Update grand total each time a line item is added,
-        accounting for delivery costs.
+        accounting for delivery costs and delete order.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        if self.order_total > settings.DISCOUNT_THRESHOLD:
-            self.discount = self.order_total * settings.PERCENTAGE_DISCOUNT/ 100
+        if self.order_total == None:
+            self.delete()
         else:
-            self.discount = 0
-        self.grand_total = self.order_total - self.discount
-        self.save()
+            if self.order_total > settings.DISCOUNT_THRESHOLD:
+                self.discount = self.order_total * settings.PERCENTAGE_DISCOUNT/ 100
+            else:
+                self.discount = 0
+            self.grand_total = self.order_total - self.discount
+            self.save()
+        
 
     def save(self, *args, **kwargs):
         """
@@ -68,7 +72,7 @@ class OrderLineItem(models.Model):
     product = models.ForeignKey(Product,
                                 null=False, blank=False, 
                                 on_delete=models.CASCADE)
-    # product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6,
                                          decimal_places=2, null=False,
@@ -82,5 +86,5 @@ class OrderLineItem(models.Model):
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
-    #def __str__(self):
-        #return f'SKU {self.product.sku} on order {self.order.order_number}'
+    def __str__(self):
+        return f'product {self.product} on order {self.order.order_number}'
