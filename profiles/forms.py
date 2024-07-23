13a00cbb3585e5b87aeed639c82c29ee48c9a698
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-07-23 20:19:26 +0000

    alters autofocus

--- profiles/forms.py
+++ profiles/forms.py
@@ -24,7 +24,7 @@ class UserProfileForm(forms.ModelForm):
             'default_country': 'Country',
         }
 
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        self.fields['default_full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
