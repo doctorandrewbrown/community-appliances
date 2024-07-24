cd7750255910c80be24ac6e56527d4d16050d1e2
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-07-24 13:22:57 +0000

    adds display volunteers view

--- volunteers/views.py
+++ volunteers/views.py
@@ -1,12 +1,23 @@
 
-# Create your views here.
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+
+from .models import VolunteerProfile
+from .forms import VolunteerProfileForm
 
 
 def volunteers(request):
     """ Display volunteer profile. """
-   
-    template = 'volunteers/volunteer.html'
+    volunteer_profile = get_object_or_404(VolunteerProfile, user=request.user)
+    if request.method == 'POST':
+        form = VolunteerProfileForm(request.POST, instance=volunteer_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Volunteer profile updated successfully')
 
-    return render(request, template)
+    form = VolunteerProfileForm(instance=volunteer_profile)
+    template = 'volunteers/volunteer.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
