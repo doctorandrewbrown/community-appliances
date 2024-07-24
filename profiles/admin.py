782be52126855568e916d120aacfdcd775d17ea6
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-07-24 09:00:03 +0000

    registers profile app

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserProfile
+
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+
+    fields = (
+            'default_full_name',
+            'default_phone_number',
+            'default_street_address1',
+            'default_street_address2',
+            'default_town_or_city',
+            'default_county',
+            'default_postcode',
+            'default_country',)
+
+admin.site.register(UserProfile, UserProfileAdmin)
