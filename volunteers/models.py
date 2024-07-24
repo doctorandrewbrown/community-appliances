9ce2147bc8aea169d4c7d022500e86577d5816e2
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-07-24 14:52:23 +0000

    switch signal on

--- volunteers/models.py
+++ volunteers/models.py
@@ -23,7 +23,7 @@ def create_or_update_volunteer_profile(sender, instance, created, **kwargs):
     """
     Create or update the volunteer profile
     """
-    #if created:
-    VolunteerProfile.objects.create(user=instance)
+    if created:
+        VolunteerProfile.objects.create(user=instance)
     #Existing users: just save the profile
-    #instance.volunteerprofile.save()
+    instance.volunteerprofile.save()
