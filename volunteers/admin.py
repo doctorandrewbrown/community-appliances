a0a56d58106bf1702a7baca97838128cae11a968
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-07-28 17:03:02 +0000

    registers volunteer profile

--- volunteers/admin.py
+++ volunteers/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import VolunteerProfile
+
+class VolunteerAdmin(admin.ModelAdmin):
+
+    fields = ('volunteer_full_name', 'email', 'details',)
+
+admin.site.register(VolunteerProfile, VolunteerAdmin)
