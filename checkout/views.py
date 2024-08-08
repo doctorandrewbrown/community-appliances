850a77f3493c0e6b31abcefe8eb00510b4156239
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-08-08 18:57:35 +0000

    adds redirect in case of form errors

--- checkout/views.py
+++ checkout/views.py
@@ -49,6 +49,7 @@ def checkout(request):
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
+            return redirect(reverse('checkout'))
     else:
         cart = request.session.get('cart', {})
         if not cart:
