f4c5a7e623c8a7d3cf76b6b703d53c894fce03cf
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-08-14 16:01:04 +0000

    remove country field

--- checkout/models.py
+++ checkout/models.py
@@ -20,7 +20,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=True, blank=True, default="UK")
+    #country = models.CharField(max_length=40, null=True, blank=True, default="UK")
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, 
                                       decimal_places=2, null=False, default=0)
