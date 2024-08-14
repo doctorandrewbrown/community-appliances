320d2cc25f0e5e03098913a7fd55001e397e258a
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-08-14 20:57:51 +0000

    remove commented out code

--- checkout/views.py
+++ checkout/views.py
@@ -72,7 +72,6 @@ def checkout(request):
                     'full_name': profile.default_full_name,
                     'email': profile.user.email,
                     'phone_number': profile.default_phone_number,
-                    #'country': profile.default_country,
                     'postcode': profile.default_postcode,
                     'town_or_city': profile.default_town_or_city,
                     'street_address1': profile.default_street_address1,
