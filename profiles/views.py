f33e525aafbd43c28ba22a68986a52c3021a29b4
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-08-16 19:27:59 +0000

    implement order history view to user

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 # direct to login page if not logged in
 @login_required
@@ -28,5 +29,20 @@ def profile(request):
         'orders': orders,
         'on_profile_page': True
     }
+
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+        }
 
     return render(request, template, context)
