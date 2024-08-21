3d7729e73f631ec45530bfc552d4a843faae5243
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-08-21 15:08:10 +0000

    amend placeholder text

--- volunteers/forms.py
+++ volunteers/forms.py
@@ -16,7 +16,7 @@ class VolunteerProfileForm(forms.ModelForm):
         # form placeholders
         placeholders = {
             'volunteer_full_name': 'Full Name',
-            'email':'email',
+            'email':'email: use the email you signed up with or a different one if you wish',
             'details':'about you'
         }
 
