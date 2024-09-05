35d2602af717952b468fd49d38f178e368814b0a
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-09-05 13:38:23 +0000

    change default product quantity to zero

--- checkout/models.py
+++ checkout/models.py
@@ -70,7 +70,7 @@ class OrderLineItem(models.Model):
                                 null=False, blank=False, 
                                 editable=False, on_delete=models.CASCADE)
     
-    quantity = models.IntegerField(null=False, blank=False, default=1)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6,
                                          decimal_places=2, null=False,
                                          blank=False, editable=False)
