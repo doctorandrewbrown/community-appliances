df76ca35ded17b7b7063f1ec510e5b1e0a64f0d2
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-09-22 22:14:57 +0000

    change email format

--- checkout/views.py
+++ checkout/views.py
@@ -136,7 +136,7 @@ def checkout_success(request, order_number):
     send_mail(
     "Subject here",
     "Checkout Success!",
-    "community-appliances@example.com",
+    "community-appliances@gmail.com",
     ["pagoni5461@cetnob.com"],
     fail_silently=False,)
 
