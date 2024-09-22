4a53837678d2c246e3c33aeb9b0357fc41108990
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-09-22 21:31:39 +0000

    set default from email value

--- checkout/views.py
+++ checkout/views.py
@@ -136,7 +136,7 @@ def checkout_success(request, order_number):
     send_mail(
     "Subject here",
     "Checkout Success!",
-    "community-appliances.com",
+    settings.DEFAULT_FROM_EMAIL,
     ["pagoni5461@cetnob.com"],
     fail_silently=False,)
 
