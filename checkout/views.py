4f1ee78973452ab754de5756ff9e8858d98285c0
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-09-22 22:09:32 +0000

    change format of default email

--- checkout/views.py
+++ checkout/views.py
@@ -136,7 +136,7 @@ def checkout_success(request, order_number):
     send_mail(
     "Subject here",
     "Checkout Success!",
-    "community-appliances.com",
+    "community-appliances@example.com",
     ["pagoni5461@cetnob.com"],
     fail_silently=False,)
 
