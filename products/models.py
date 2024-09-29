a51ccd53c3a39f8e7bd523c44cf220ccc021dbc0
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-09-29 18:35:36 +0000

    remove unused model fields and functions. Change image and description constraints in Product model

--- products/models.py
+++ products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Certificate(models.Model):
+    """
+    A Certificate model for maintaining details of appliance safety
+    certificates
+    """
     certificate = models.CharField(max_length=254)
     description = models.TextField(null=True, blank=True)
 
@@ -12,49 +16,50 @@ class Certificate(models.Model):
 
 
 class Condition(models.Model):
+    """
+    A  model for maintaining details of appliance condition A-C
+    """
     class Meta:
         verbose_name_plural = "Condition"
     grade = models.CharField(max_length=254)
     description = models.TextField(null=True, blank=True)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     # string representations of model
     def __str__(self):
         return self.grade
 
-    def get_friendly_name(self):
-        return self.friendly_name
-
 
 class Category(models.Model):
+    """
+    A Category model for maintaining 
+    details of appliance categories
+    """
 
     # corrects plural in admin view
     class Meta:
         verbose_name_plural = "Categories"
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     # string representations of model
     def __str__(self):
         return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
-
 
 class Product(models.Model):
+    """
+    A Product model for maintaining appliance details
+    """
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True,)
     grade = models.ForeignKey('Condition', null=True, blank=True,
                               on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     certificate = models.ForeignKey('Certificate', null=True, blank=True,
                                     on_delete=models.SET_NULL)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField()
 
     # string representations of model
     def __str__(self):
