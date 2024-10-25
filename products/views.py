4bf1c71cf7fe5b6ae7e7af96891ae4edd31b4ea8
Andrew Brown <dr.andrew.david.brown@gmail.com>
2024-10-25 19:54:46 +0000

    add login required to delete

--- products/views.py
+++ products/views.py
@@ -134,6 +134,8 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
+
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
